ded8056a8e797c75ed911b0df5d5c8bfb7b12f0d
Isaac Reis <isaac.reis@teknisa.com>
2024-12-06 17:13:14 -0300

    melhorando estilo dos elementos da pagina com uma animacao suave junto com scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,15 @@ $(document).ready(() => {
       }
       this.sendForm = this.sendForm.bind(this);
     }
-  
+
     displaySuccess() {
       this.form.innerHTML = this.settings.success;
     }
-  
+
     displayError() {
       this.form.innerHTML = this.settings.error;
     }
-  
+
     getFormObject() {
       const formObject = {};
       const fields = this.form.querySelectorAll("[name]");
@@ -26,30 +26,34 @@ $(document).ready(() => {
       });
       return formObject;
     }
-  
+
     onSubmission(event) {
       event.preventDefault();
       event.target.disabled = true;
       event.target.innerText = "Enviando...";
     }
-  
+
     checkFieldsSubmission() {
       const fields = this.getFormObject();
       let bool = true;
       if (fields.nome == "" || !fields.nome) {
         bool = false;
-      } else if (fields.email == "" || !fields.email || !fields.email.includes("@")) {
+      } else if (
+        fields.email == "" ||
+        !fields.email ||
+        !fields.email.includes("@")
+      ) {
         bool = false;
       } else if (fields.mensagem == "" || !fields.mensagem) {
         bool = false;
       }
       return bool;
     }
-  
+
     async sendForm(event) {
       try {
         if (this.checkFieldsSubmission()) {
-          console.log("Aqui")
+          console.log("Aqui");
           this.onSubmission(event);
           await fetch(this.url, {
             method: "POST",
@@ -60,19 +64,19 @@ $(document).ready(() => {
             body: JSON.stringify(this.getFormObject()),
           });
           this.displaySuccess();
-        };
+        }
       } catch (error) {
         this.displayError();
         throw new Error(error);
       }
     }
-  
+
     init() {
       if (this.form) this.formButton.addEventListener("click", this.sendForm);
       return this;
     }
   }
-  
+
   const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
@@ -81,18 +85,41 @@ $(document).ready(() => {
   });
   formSubmit.init();
 
-  $('.cabecalho .cabecalho__menu-mobile svg').on('click', () => {
+  $(".cabecalho .cabecalho__menu-mobile svg").on("click", () => {
     console.log("click");
-    $('.cabecalho__menu-mobile--nav').css('display','flex');
-  })
+    $(".cabecalho__menu-mobile--nav").css("display", "flex");
+  });
 
-  $('.cabecalho__menu-mobile--fechar').on('click', () => {
+  $(".cabecalho__menu-mobile--fechar").on("click", () => {
     console.log("click");
-    $('.cabecalho__menu-mobile--nav').css('display','none');
-  })
+    $(".cabecalho__menu-mobile--nav").css("display", "none");
+  });
 
-  $('.cabecalho__navMobilelink').on('click', () => {
+  $(".cabecalho__navMobilelink").on("click", () => {
     console.log("click");
-    $('.cabecalho__menu-mobile--nav').css('display','none');
-  })
+    $(".cabecalho__menu-mobile--nav").css("display", "none");
+  });
+
+  // Função para verificar se o elemento está visível na tela
+  function isElementInView(el) {
+    const rect = el.getBoundingClientRect();
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+
+    const elementVisible = rect.top <= windowHeight * 0.75;
+
+    return elementVisible;
+  }
+
+  window.addEventListener("scroll", function () {
+    const elements = document.querySelectorAll(".animate");
+
+    elements.forEach(function (element) {
+      if (isElementInView(element)) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+  });
 });
